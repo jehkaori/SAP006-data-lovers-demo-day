a85f57131a4ad37bb4810b3e3ab5be08d4614dbf
Jéssica Inamassu <inamassu.kaori@gmail.com>
2021-08-25 21:42:51 -0300

    Img not-found

--- src/notfound/index.js
+++ src/notfound/index.js
@@ -7,7 +7,7 @@ export const notFound = () => {
     <div class="terminal">
       <h1>Error <span class="errorcode">404</span></h1>
       <p class="output">A página que você está procurando pode ter sido removida, teve seu nome alterado ou está temporariamente indisponível.</p>
-      <p class="output">Por favor, tente <a href="/home">retornar para homepage</a>.</p>
+      <p class="output">Por favor, tente <a class="link-home" href="/home">retornar para homepage</a>.</p>
       </div>
       <div class="img-container">
         <img class="img-not" src="img/rick-and-morty.png" alt="">
