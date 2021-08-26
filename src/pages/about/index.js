6184c85ae65f7732e0a4d19139bdeee2687cffd7
Bianca Cristina Alves <biancacristinaalves@hotmail.com>
2021-08-26 14:17:52 -0300

    imagem netflix

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -20,7 +20,7 @@ export const About = () => {
           <a href="https://www.netflix.com/br/title/80014749#:~:text=Rick%20and%20Morty%20%7C%20Netflix" target="_blank"><img class="netflix" src="img/netflix.png" title="Assistir em Netflix" alt="Netflix"></a>
           <a href="https://www.hbomax.com/br/pt/rick-and-morty?utm_id=sa%7c71700000085970731%7c58700007304447408%7cp65554600674&gclid=CjwKCAjw95yJBhAgEiwAmRrutOZQ2vdQd92wTFIhKxf_D9ygOXAFxIPKD1JRPd_mmXF_ntKE7p4juhoCykAQAvD_BwE&gclsrc=aw.ds&countryRedirect=1" target="_blank"><img class="hbomax" src="img/hbomax.png" title="Assistir em HBOMax" alt="HBOMax"></a>
   `;
-
+  
   container.querySelector('#back-btn').addEventListener('click', (e) => { 
     e.preventDefault();
     navigation('/home');
